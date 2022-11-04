2539fed8339bc54fca770028cb7512be6a5d5502
Vitalij Vladimirov <45896500+vitalij-vladimirov@users.noreply.github.com>
2022-11-04 11:12:36 +0200

    Create vendor-test.sh

---
+++ vendor-test.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# LOCAL VENDOR TESTING KNOW HOW
+#
+# To test library locally it needs to be installed (composer require) and then mocked from local location as a symlink.
+# This script helps to create symlink running less commands.
+#   1. Run `composer install` if library is not installed yet.
+#   2. Run `./vendor-test.sh library-name` to mock library (specify your library instead of `library-name`)
+# To rollback original library version run `composer install` again.
+#
+# Your must include volume specified in LIBS_DIR and run it in the same directory inside of the image.
+# In any other cases symlink will not work inside of docker image.
+# docker-compose.override.yml example:
+# > version: "3"
+# >  services:
+# >    php:
+# >      volumes:
+# >        - /home/user/projects/libs:/home/user/projects/libs
+
+LIBS_DIR=~/projects/libs/ # Specify path to local location of library
+VENDOR_DIR="opay-dev"     # Specify vendor name where testing libraries must be stored
+
+if [ -z "$1" ]; then
+  echo -e "\033[31mSpecify library name.\033[0m";
+  exit
+fi
+
+FULL_VENDOR_PATH="${PWD}/vendor/${VENDOR_DIR}"
+if [ ! -d "$FULL_VENDOR_PATH" ]; then
+  echo -e "\033[31mVendor directory \"$FULL_VENDOR_PATH\" not found.\033[0m"
+  echo "Run \`composer install\` before vendor testing."
+  exit
+fi
+
+TEST_LIB_DIR="${LIBS_DIR}${1}"
+if [ ! -d "$TEST_LIB_DIR" ]; then
+  echo -e "\033[31mSpecified library \"${1}\" not found in ${LIBS_DIR}\033[0m"
+  exit
+fi
+
+TEST_LIB_VENDOR_DIR="${VENDOR_DIR}${1}"
+if [ -d "$TEST_LIB_VENDOR_DIR" ]; then
+  rm -rf "$TEST_LIB_VENDOR_DIR"
+fi
+
+ln -s "$TEST_LIB_DIR" "$TEST_LIB_VENDOR_DIR"
+
+echo -e "\033[32mLibrary testing symlink created!\033[0m"
